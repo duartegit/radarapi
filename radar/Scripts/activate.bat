e3eb03229d28ed9ee27afe2bdaf4a501601dc8b0
duartegit <lucasduarte.2009@hotmail.com>
2021-10-29 12:47:37 -0300

    comit image readme

--- radar/Scripts/activate.bat
+++ radar/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\lucas\flask-sqlalchemy\radar
+set VIRTUAL_ENV=C:\Users\administrator\Desktop\flask-sqlalchemy\radar
 
 if not defined PROMPT set PROMPT=$P$G
 
